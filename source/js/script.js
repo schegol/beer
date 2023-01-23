79500966703c8cf429373e6b4826149a4a58899a
VladProfitkit <vlad@profitkit.ru>
2023-01-23 22:25:16 +0400

    Завершение главной

--- source/js/script.js
+++ source/js/script.js
@@ -19,6 +19,49 @@ $(function () {
             });
         });
     }
+
+    //избранное (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
+    let productFavBtns = $('.product__corner-btn--fav');
+    
+    productFavBtns.each(function() {
+        let btn = $(this);
+
+        btn.on('click', function() {
+            btn.toggleClass('product__corner-btn--fav-active');
+        });
+    });
+
+    //имитация счетчика товара (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
+    let productControlsBlock = $('.product__controls');
+
+    productControlsBlock.each(function() {
+        let block = $(this),
+            btns = block.find('.product__controls-btn');
+
+        btns.on('click', function() {
+            let btn = $(this),
+                valueField = block.find('.product__controls-counter'),
+                value = parseInt(valueField.val()),
+                newValue;
+
+            if (!btn.hasClass('product__controls-btn--disabled')) {
+                if (btn.hasClass('product__controls-btn--plus')) {
+                    newValue = value+1;
+                } else {
+                    newValue = value-1;
+                }
+
+                valueField.val(newValue);
+                if (newValue > 0) {
+                    btns.removeClass('product__controls-btn--disabled');
+                    block.addClass('product__controls--active');
+                } else if (newValue === 0) {
+                    btns.filter('.product__controls-btn--minus').addClass('product__controls-btn--disabled');
+                    block.removeClass('product__controls--active');
+                }
+            }
+        });
+    });
 });
 
 $(window).on('load', function() {
