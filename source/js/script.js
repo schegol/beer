e52c0af1b0b490d480cd3ecfc01d652f72f7a3e1
VladProfitkit <vlad@profitkit.ru>
2023-01-26 03:01:36 +0400

    Еще несколько страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,22 @@
+//модалки:
+let body = $('body'),
+    modalOverlay = $('.modal'),
+    modals = $('.modal__body');
+
+const openModal = function(id) {
+    let targetModal = modalOverlay.children('#'+id);
+
+    body.addClass('fixed');
+    modalOverlay.addClass('modal--open');
+    targetModal.addClass('modal__body--open');
+}
+
+const closeModals = function() {
+    body.removeClass('fixed');
+    modalOverlay.removeClass('modal--open');
+    modals.removeClass('modal__body--open');
+}
+
 $(function () {
 	//слайдеры:
     let sliders = $('.slider__items');
@@ -20,68 +39,138 @@ $(function () {
         });
     }
 
-    //избранное (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
-    let productBtns = $('.product__corner-btn');
+    //избранное и удаление товара (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
+    let productBtns = $('.product__corner-btn'),
+        elementFavBtn = $('.catalog-element__fav-btn');
     
-    productBtns.each(function() {
-        let btn = $(this);
-
-        btn.on('click', function() {
-            if (btn.hasClass('product__corner-btn--fav')) {
-                btn.toggleClass('product__corner-btn--fav-active');
-            } else if (btn.hasClass('product__corner-btn--remove')) {
-                let product = btn.closest('.product');
-
-                product.remove();
-            }
+    if (productBtns.length) {
+        productBtns.each(function() {
+            let btn = $(this);
+    
+            btn.on('click', function() {
+                if (btn.hasClass('product__corner-btn--fav')) {
+                    btn.toggleClass('product__corner-btn--fav-active');
+                } else if (btn.hasClass('product__corner-btn--remove')) {
+                    let product = btn.closest('.product');
+    
+                    product.remove();
+                }
+            });
         });
-    });
+    }
+
+    if (elementFavBtn.length) {
+        elementFavBtn.on('click', function() {
+            $(this).toggleClass('catalog-element__fav-btn--active');
+        });
+    }
 
     //имитация счетчика товара (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
     let productControlsBlock = $('.product__controls');
 
-    productControlsBlock.each(function() {
-        let block = $(this),
-            btns = block.find('.product__controls-btn');
-
-        btns.on('click', function() {
-            let btn = $(this),
-                valueField = block.find('.product__controls-counter'),
-                value = parseInt(valueField.val()),
-                newValue;
-
-            if (!btn.hasClass('product__controls-btn--disabled')) {
-                if (btn.hasClass('product__controls-btn--plus')) {
-                    newValue = value+1;
-                } else {
-                    newValue = value-1;
+    if (productControlsBlock.length) {
+        productControlsBlock.each(function() {
+            let block = $(this),
+                btns = block.find('.product__controls-btn');
+    
+            btns.on('click', function() {
+                let btn = $(this),
+                    valueField = block.find('.product__controls-counter'),
+                    value = parseInt(valueField.val()),
+                    newValue;
+    
+                if (!btn.hasClass('product__controls-btn--disabled')) {
+                    if (btn.hasClass('product__controls-btn--plus')) {
+                        newValue = value+1;
+                    } else {
+                        newValue = value-1;
+                    }
+    
+                    valueField.val(newValue);
+                    if (newValue > 0) {
+                        btns.removeClass('product__controls-btn--disabled');
+                        block.addClass('product__controls--active');
+                    } else if (newValue === 0) {
+                        btns.filter('.product__controls-btn--minus').addClass('product__controls-btn--disabled');
+                        block.removeClass('product__controls--active');
+                    }
                 }
-
-                valueField.val(newValue);
-                if (newValue > 0) {
-                    btns.removeClass('product__controls-btn--disabled');
-                    block.addClass('product__controls--active');
-                } else if (newValue === 0) {
-                    btns.filter('.product__controls-btn--minus').addClass('product__controls-btn--disabled');
-                    block.removeClass('product__controls--active');
-                }
-            }
+            });
         });
-    });
+    }
 
     //открытие списков товаров:
-    let productsListHeads = $('.products-list__block-head');
+    let productsListHeads = $('.products-list__block-head'),
+        aminationInProgress = false;
 
-    productsListHeads.each(function() {
-        let head = $(this),
-            products = head.next('.products-list__products');
-
-        head.on('click', function(e) {
-            e.preventDefault;
-
-            head.toggleClass('products-list__block-head--open');
-            products.slideToggle(300);
+    if (productsListHeads.length) {
+        productsListHeads.each(function() {
+            let head = $(this),
+                products = head.next('.products-list__products');
+    
+            head.on('click', function(e) {
+                e.preventDefault;
+                if (!aminationInProgress) {
+                    aminationInProgress = true;
+    
+                    head.toggleClass('products-list__block-head--open');
+                    products.slideToggle(300);
+    
+                    setTimeout(function() {
+                        aminationInProgress = false;
+                    }, 300);
+                }
+            });
         });
+    }
+
+    //имитация поиска по товарам (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
+    let searchField = $('.catalog-filters__search'),
+        resultBlock = $('.catalog-filters__results'),
+        products = $('.products-list'),
+        resetBtn = $('.catalog-filters__reset-btn'),
+        goodsNotFoundBlock = $('.no-products');
+
+    const cancelFiltering = function() {
+        products.show();
+        resultBlock.hide();
+        goodsNotFoundBlock.hide();
+    }
+
+    if (searchField.length) {
+        searchField.on('keyup', function() {
+            if (searchField.val() != '') {
+                products.hide();
+                resultBlock.css('display', 'flex');
+                goodsNotFoundBlock.css('display', 'flex');
+            } else {
+                cancelFiltering();
+            }
+        });
+
+        resetBtn.on('click', function() {
+            cancelFiltering();
+        });
+    }
+
+    //вызовы модалкок (УДАЛИТЬ ПОСЛЕ ПЕРЕНОСА):
+    let sortModalCallBtn = $('.catalog-filters__sort-btn'),
+        closeModalBtns = $('.modal__close-btn'),
+        modalsOverlay = $('.modal'),
+        modals = $('.modal__body');
+
+    sortModalCallBtn.on('click', function() {
+        openModal('modalFilter');
+    });
+
+    closeModalBtns.on('click', function() {
+        closeModals();
+    });
+
+    modalsOverlay.click(function (e) {
+        if (!modals.is(e.target) && modals.has(e.target).length === 0) {
+            closeModals();
+        };
     });
 });
 
